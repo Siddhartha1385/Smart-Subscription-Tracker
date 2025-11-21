1d9b21802649da6c451149b2d206580afbd85fe0
Siddhartha1385 <2022UCP1385@mnit.ac.in>
2025-11-21 22:50:38 +0530

    Set production API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 // src/api/axios.js
 import axios from "axios";
 
-const base = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+const base = import.meta.env.VITE_API_BASE_URL || "https://smart-subscription-tracker-7izi.onrender.com/api";
 
 const api = axios.create({
   baseURL: base,
